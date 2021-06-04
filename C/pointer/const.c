e25c933838acf977b34ccf1b3ced6133de4d2868
Tony-C11 <2014898200@qq.com>
2021-06-04 20:44:50 +0800

    file-commit

--- C/pointer/const.c
+++ C/pointer/const.c
@@ -7,6 +7,13 @@ void show_arr(const int * ptr);
 int main(void)
 {
     int arr[SIZE] = {1, 2, 3, 4, 5};
+    show_arr(arr);
+    const int num = 5;
+    const int * ptr1 = &num;
+    int * ptr2 = &num;
+    printf("%d, %d\n", *ptr2, *ptr1);
+    *ptr2 = 1;
+    printf("%d, %d\n", *ptr2, *ptr1);
     return 0;
 }
 
