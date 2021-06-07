2cc6e26db8c493037bc37051fce66a9f523865c0
Tony-C11 <2014898200@qq.com>
2021-06-07 13:41:02 +0800

    file-commit

---
+++ C/pointer/var_arr.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int row;
+    int col;
+    int rows;
+    int cols;
+    printf("请输入你想创建的数组的“行数”和“列数”：");
+    scanf("%d%d", &rows, &cols);
+    int arr[rows][cols];
+    // 变长数组
+
+    for (row = 0; row < rows; ++row)
+        for(col = 0; col < cols; ++col)
+        {
+            arr[row][col] = 100;        
+        }
+
+    for (row = 0; row < rows; ++row)
+    {    
+        for(col = 0; col < cols; ++col)
+        {
+            printf("%d  ", arr[row][col]);
+        }
+        putchar('\n');
+    }
+
+
+    return 0;    
+}
