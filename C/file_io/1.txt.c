32424ee3c25f398cc6a26089e608c66d01de2499
Tony-C11 <2014898200@qq.com>
2021-06-10 19:28:08 +0800

    file-commit

--- C/file_io/1.txt.c
+++ C/file_io/1.txt.c
@@ -20,3 +20,25 @@ asdasdasdsad
 
 
 asdasdasdasdaddsadasdasdasdasdasdasdasdsdasdasdasdasd
+asdasdasdasdsadasdadsadas
+asdasdasdasdasdasdsad
+asdasdsadsadasdas
+
+asdasdasdasdasd
+
+asdasdasdsad
+
+
+
+asdasdasdasdaddsadasdasdasdasdasdasdasdsdasdasdasdasd
+asdasdasdasdsadasdadsadas
+asdasdasdasdasdasdsad
+asdasdsadsadasdas
+
+asdasdasdasdasd
+
+asdasdasdsad
+
+
+
+asdasdasdasdaddsadasdasdasdasdasdasdasdsdasdasdasdasd
--- C/file_io/fopen.c
+++ C/file_io/fopen.c
@@ -1,8 +1,10 @@
+// 拷贝一份文件数据，将参数1拷贝到新文件中（以.c结尾），不覆盖参数2原来的数据
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define NAME    20
-
+#define INFO    1000
 int main(int argc, char * argv[])
 {
     FILE * fp1;
@@ -10,6 +12,7 @@ int main(int argc, char * argv[])
     unsigned long count = 0;
     char ch;
     char file[NAME];
+    char newFile[INFO];
 
     if (argc < 2)
     {
@@ -23,20 +26,40 @@ int main(int argc, char * argv[])
         exit(EXIT_FAILURE);
     }
 
-    if ((fp2 = fopen(argv[2], "w")) == NULL)
+    // 做一个副本，以 .c做后缀
+    strncpy(file, argv[1], NAME - 3);
+    file[NAME - 3] = '\0';
+    // 如果源文件名字太长，拷贝到file[NAME-4]的位置也没拷贝完，就在file[NAME-4]的位置添加一个'\0'，因为strcat()从第一个空字符开始拼接。
+    if ((fp2 = fopen(strcat(file, ".c"), "a+")) == NULL)
     {
-        fprintf(stderr, "We can't open this %s!\n", argv[2]);
+        fprintf(stderr, "We can't creat the new file!\n");
         exit(EXIT_FAILURE);
     }
 
     while ((ch = getc(fp1)) != EOF)
     {
         putc(ch, fp2);
+        putc(ch, stdout);   
         count++;
     }
     
     printf("%s has %d characters!!!\n", argv[1], count);
-    
+    puts("在新文件中的信息是：");
+    // while (getc)
+    // 此时已经到了源文件和新文件的末尾
+     rewind(fp2);   // 重新定位到文件开头
+    while (fgets(newFile, INFO, fp2) != NULL)   // 读到文件结尾返回NULL
+    {
+        fputs(newFile, stdout);
+    }
+
+    fclose(fp1);
+    fclose(fp2);
 
     return 0;
 }
+
+
+
+
+
