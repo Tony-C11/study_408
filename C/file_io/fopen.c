f83ff3ffe1c91bcd0fce2da6caa7cde2e3cb39e8
Tony-C11 <2014898200@qq.com>
2021-06-10 17:57:44 +0800

    file-commit

--- C/file_io/fopen.c
+++ C/file_io/fopen.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define NAME    20
 
 int main(int argc, char * argv[])
 {
-    printf("There're %d arguments!\n", argc - 1);
-    int count = 1;
-    if (argc - 1 == 0)
-        printf("No argument!!!\n");
-    else
-        while (count < argc)
-        {
-            puts(argv[count]);
-            count++;
-        }
+    FILE * fp1;
+    FILE * fp2;
+    unsigned long count = 0;
+    char ch;
+    char file[NAME];
+
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s filename.\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if ((fp1 = fopen(argv[1], "r")) == NULL)
+    {
+        fprintf(stderr, "We can't open this %s!\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    if ((fp2 = fopen(argv[2], "w")) == NULL)
+    {
+        fprintf(stderr, "We can't open this %s!\n", argv[2]);
+        exit(EXIT_FAILURE);
+    }
+
+    while ((ch = getc(fp1)) != EOF)
+    {
+        putc(ch, fp2);
+        count++;
+    }
     
+    printf("%s has %d characters!!!\n", argv[1], count);
+    
+
     return 0;
 }
